1a39c144556cf5d8d1cc242da5e94ceefae97c4f
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-04-13 17:28:28 +0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+
+console.log()
+
+module.exports = (env = {}) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          exclude: /node_modules/i,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.html/i,
+          use: [{
+            loader: 'html-loader'
+          }]
+        }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      },
+      minimize: !!env.production,
+      minimizer: [ new TerserWebpackPlugin() ]
+    },
+    devtool: 'cheap-eval-source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      }),
+      new CleanWebpackPlugin()
+    ]
+  }
+}
